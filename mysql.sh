c7a4f4fb767e729a7aef29d47ccc28a6468a293b
Rogerio Guimaraes <rogerio@dx4all.com.br>
2021-03-15 21:27:07 -0300

    changes

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y 
+sudo apt-get install git -y
 sudo apt-get install mysql-server -y
 sudo systemctl status mysql
+sudo git clone https://github.com/v-rguimaraes/az104.git
+cd az104
+sudo mysql > wordpress.sql
